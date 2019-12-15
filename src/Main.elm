3fa01d5f0ef540c13a7b968f33b820954c570f24
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2019-12-15 10:32:13 +0530

    Made this as a elm project. Going to update this whole website

---
+++ src/Main.elm
@@ -0,0 +1,46 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
