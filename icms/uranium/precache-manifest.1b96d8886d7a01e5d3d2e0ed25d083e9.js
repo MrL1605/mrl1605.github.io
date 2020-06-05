c43f00101f7a88118c10ac44c3a96cb66a2bd019
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-06-05 16:01:44 +0530

    Update to add Uranium

---
+++ icms/uranium/precache-manifest.1b96d8886d7a01e5d3d2e0ed25d083e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c624f73239b51717409914934399dddc",
+    "url": "/icms/uranium/index.html"
+  },
+  {
+    "revision": "6baec8ccec41a9c2ef24",
+    "url": "/icms/uranium/static/css/2.72d6b061.chunk.css"
+  },
+  {
+    "revision": "6fd5d069cd4cda99265c",
+    "url": "/icms/uranium/static/css/main.b521074a.chunk.css"
+  },
+  {
+    "revision": "6baec8ccec41a9c2ef24",
+    "url": "/icms/uranium/static/js/2.3421389c.chunk.js"
+  },
+  {
+    "revision": "6fd5d069cd4cda99265c",
+    "url": "/icms/uranium/static/js/main.bfb230c9.chunk.js"
+  },
+  {
+    "revision": "a216c3e8fa962ccf3fc8",
+    "url": "/icms/uranium/static/js/runtime~main.800989d2.js"
+  }
+]);
