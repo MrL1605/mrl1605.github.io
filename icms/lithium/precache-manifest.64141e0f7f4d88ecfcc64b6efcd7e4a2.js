80b3c61bab36a44b8f45b7dc3313b970a238d310
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-06-08 15:17:14 +0530

    Updated ICMS index and Added Lithium to the project

---
+++ icms/lithium/precache-manifest.64141e0f7f4d88ecfcc64b6efcd7e4a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b627ce34ee35e9687b05a2a0b1090f1d",
+    "url": "/icms/lithium/index.html"
+  },
+  {
+    "revision": "8ab5970c5329ff6532f9",
+    "url": "/icms/lithium/static/css/main.3e32605d.chunk.css"
+  },
+  {
+    "revision": "7de77e0e68e2e001edf1",
+    "url": "/icms/lithium/static/js/2.5a58d949.chunk.js"
+  },
+  {
+    "revision": "8ab5970c5329ff6532f9",
+    "url": "/icms/lithium/static/js/main.1f590be0.chunk.js"
+  },
+  {
+    "revision": "ee36434b4b005c7bab47",
+    "url": "/icms/lithium/static/js/runtime~main.1dcfb80e.js"
+  }
+]);
