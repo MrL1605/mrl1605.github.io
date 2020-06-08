3b4c8c92753dc10dcdd33f47d0a7e3689265cb77
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-06-08 17:47:53 +0530

    Update to add MDB

---
+++ icms/oxygen/precache-manifest.f5908495c241583ba4f21c37e9b74c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed9a91dc82ff9e19af9a0229788e759",
+    "url": "/icms/oxygen/index.html"
+  },
+  {
+    "revision": "410aa82d921832b3f862",
+    "url": "/icms/oxygen/static/css/main.3e32605d.chunk.css"
+  },
+  {
+    "revision": "7de77e0e68e2e001edf1",
+    "url": "/icms/oxygen/static/js/2.5a58d949.chunk.js"
+  },
+  {
+    "revision": "410aa82d921832b3f862",
+    "url": "/icms/oxygen/static/js/main.7fa70f30.chunk.js"
+  },
+  {
+    "revision": "d41528456416804326e7",
+    "url": "/icms/oxygen/static/js/runtime~main.73e30eef.js"
+  }
+]);
