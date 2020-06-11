751925bab40c32666a7a3406e3ce571c4dc7c6f1
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-06-11 20:08:52 +0530

    Update to add germanium. Getting bored of this now

---
+++ icms/germanium/precache-manifest.ee31e91b564c3345ab69a705976c109c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f686aef7d3e24dccb1ef2cef466b6d",
+    "url": "/icms/germanium/index.html"
+  },
+  {
+    "revision": "acde7f5306764116331e",
+    "url": "/icms/germanium/static/css/main.9a81d2a4.chunk.css"
+  },
+  {
+    "revision": "36b8e1ec42cb82a056d0",
+    "url": "/icms/germanium/static/js/2.a09b4f4f.chunk.js"
+  },
+  {
+    "revision": "acde7f5306764116331e",
+    "url": "/icms/germanium/static/js/main.9ede135c.chunk.js"
+  },
+  {
+    "revision": "0004d0bd620aebfeb4d2",
+    "url": "/icms/germanium/static/js/runtime~main.5afc1b81.js"
+  }
+]);
