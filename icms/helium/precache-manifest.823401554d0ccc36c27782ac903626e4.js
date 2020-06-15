a1780a6389c2d37d2919f0a1882dd227860c03ad
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2020-06-15 16:15:22 +0530

    Update to add Aboard theme

---
+++ icms/helium/precache-manifest.823401554d0ccc36c27782ac903626e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3be0dd403bf32d92ca9b2dfc656615a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a630b0b6e737e7ac86a",
+    "url": "/static/css/2.ea4bfe69.chunk.css"
+  },
+  {
+    "revision": "7149fdabb4c58f886793",
+    "url": "/static/css/main.aa019d41.chunk.css"
+  },
+  {
+    "revision": "5a630b0b6e737e7ac86a",
+    "url": "/static/js/2.d3fee5c6.chunk.js"
+  },
+  {
+    "revision": "7149fdabb4c58f886793",
+    "url": "/static/js/main.5dedb403.chunk.js"
+  },
+  {
+    "revision": "c97ce9e7578361ecfb1a",
+    "url": "/static/js/runtime~main.b0127790.js"
+  },
+  {
+    "revision": "803c32422f80cb17dc71f4d5e9ccac2f",
+    "url": "/static/media/illustration-404.803c3242.svg"
+  },
+  {
+    "revision": "af66f14deb0eaa75e09ad7c0fb69bff2",
+    "url": "/static/media/logo.af66f14d.svg"
+  },
+  {
+    "revision": "879a8310c7b1f4fdc2c8c0ff9623d2fc",
+    "url": "/static/media/screen-bg.879a8310.jpg"
+  }
+]);
